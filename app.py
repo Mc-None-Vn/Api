22198b0302d31814c13b16e3b75f6fecbf4389f3
Marlernix Mc <letloc3621@gmail.com>
2025-04-14 15:10:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import requests
 
 app = FastAPI()
+class Items = BaseModel():
+    name: str
+    avt: str
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the root route!"}
 
-@app.get("/image")
-def image(image: str):
+@app.port("/image")
+def image(item: Items):
     time = 15552000
-    name = "test"
+    name = item.name
+    image = item.image
     key = c16ec93af2dfad387e2f685867d4d338
     url = "https://api.imgbb.com/1/upload?expiration=" + time + "&key=" + key + "&image=" + image + "&name=" + name
     req = requests.get(url)
