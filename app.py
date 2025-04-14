d54f351cafb491f842afc19df3b7e1c9042ac944
Marlernix Mc <letloc3621@gmail.com>
2025-04-14 15:01:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Welcome to the root route!"}
+
+@app.get("/image")
+def image(image: str):
+    time = 15552000
+    name = "test"
+    key = c16ec93af2dfad387e2f685867d4d338
+    url = "https://api.imgbb.com/1/upload?expiration=" + time + "&key=" + key + "&image=" + image + "&name=" + name
+    req = requests.get(url)
+    return req.json()
