0918911248aea3bef1b28932e91ba4b8883e69c8
Mc-None-Vn <letloc3621@gmail.com>
2025-04-15 17:06:23 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import requests
+
+class imgbb(BaseModel):
+  name: str
+  image: str
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
-  return {"status": 200, "message": "Welcome to Mc Api"}
+  return {"message": "Welcome to Mc Api"}
 
+@app.post("/img/")
+async def img(item: imgbb):
+  return ("message": {"name": item.name, "image": item.image})
