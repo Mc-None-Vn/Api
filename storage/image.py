bcbe86a67218177cd40562e5b0a7af0c942d76b3
Mc-None-Vn <letloc3621@gmail.com>
2025-04-16 16:07:55 +0700

    update image.py

--- storage/image.py
+++ storage/image.py
@@ -4,15 +4,36 @@ from pydantic import BaseModel
 import requests
 import uuid
 import os
+import re
 
 router = APIRouter()
+
 class ImgRequest(BaseModel):
     image: str
+    time: str = "1d"
+
+def parse_time(time_str):
+    pattern = r"(\d+)([dDhHmMsS])"
+    matches = re.findall(pattern, time_str, re.IGNORECASE)
+    seconds = 0
+    units = {
+        's': 1,
+        'm': 60,
+        'h': 3600,
+        'd': 86400
+    }
+    for value, unit in matches:
+        unit = unit.lower()
+        if unit in units:
+            seconds += int(value) * units[unit]
+        else:
+            seconds = 86400
+    return seconds
 
 @router.post("/api/image")
 async def img(item: ImgRequest):
     try:
-        ex = 60 * 60 * 24
+        ex = parse_time(item.time)
         name = str(uuid.uuid4())
         key = os.environ.get("IMGBB_KEY")
         image_data = requests.get(item.image).content
@@ -25,13 +46,12 @@ async def img(item: ImgRequest):
             },
             files={"image": image_data}
         )
-
         if response.status_code == 200:
             data = response.json()
             url = data["data"]["url"]
             if not url.endswith(".png"):
                 filename, file_extension = os.path.splitext(url)
-                url = filename + ".png"            
+                url = filename + ".png"
             return JSONResponse({"image": item.image, "url": url}, status_code=200)
         else:
             return JSONResponse({"error": "Error uploading image to cloud storage"}, status_code=400)
