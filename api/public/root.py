37051dc0d24d4accb4641183400d95157717428c
Mc-None-Vn <letloc3621@gmail.com>
2025-04-17 17:38:40 +0700

    update

---
+++ api/public/root.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
+import json
+import os
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), "../../storage/data.json")) as f:
+            data = json.load(f)
+        if 'url' not in data:
+            raise HTTPException(status_code=500, detail="URL not found in data.json")
+        return RedirectResponse(url=f"{data['url']}/docs/")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
