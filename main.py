d239b3b0d091022afd1fc4b9d71981741773df9f
Mc-None-Vn <letloc3621@gmail.com>
2025-04-18 21:49:00 +0700

    update main.py

--- main.py
+++ main.py
@@ -4,18 +4,47 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 import json
 
-with open("./storage/data.json") as d:
-    data = json.load(d)
+def load_data(file_path):
+    with open(file_path) as f:
+        return json.load(f)
 
-app = FastAPI(
-    title=f"{data['title']}",
-    description=f"{data['description']}",
-    version=f"{data['version']}",
-    docs_url="/website/docs/",
-)
+def fast_api(data):
+    return FastAPI(
+        title=data['title'],
+        description=data['description'],
+        version=data['version'],
+        docs_url="/website/docs/",
+    )
 
-with open("./storage/no_key.json") as f:
-    exempt_request = json.load(f)
+def run_router(app, folder):
+    for FOLDER in folder:
+        route_dir = os.path.join(os.path.dirname(__file__), FOLDER)
+        for filename in os.listdir(route_dir):
+            if filename.endswith(".py") and filename != "__init__.py":
+                module_name = filename[:-3]
+                module = importlib.import_module(f"{FOLDER}.{module_name}")
+                if hasattr(module, "router"):
+                    try:
+                        app.include_router(module.router)
+                    except Exception as e:
+                        print(f"Lỗi khi include router {module_name}: {e}")
+        for subfolder in os.listdir(route_dir):
+            subfolder_path = os.path.join(route_dir, subfolder)
+            if os.path.isdir(subfolder_path):
+                for filename in os.listdir(subfolder_path):
+                    if filename.endswith(".py") and filename != "__init__.py":
+                        module_name = filename[:-3]
+                        module = importlib.import_module(f"{FOLDER}.{subfolder}.{module_name}")
+                        if hasattr(module, "router"):
+                            try:
+                                app.include_router(module.router, prefix=f"/{FOLDER}/{subfolder}")
+                            except Exception as e:
+                                print(f"Lỗi khi include router {module_name}: {e}")
+
+data = load_data("./storage/data.json")
+exempt_request = load_data("./storage/no_key.json")
+
+app = fast_api(data)
 
 @app.middleware("http")
 async def check_header(request: Request, call_next):
@@ -36,33 +65,8 @@ async def check_header(request: Request, call_next):
         if request.headers["key"] != str(os.environ.get("API_Key")):
             return JSONResponse({"error": "Api key does not exist"}, status_code=401)
     return await call_next(request)
-
-def run_router(folder):
-    for FOLDER in folder: 
-        route_dir = os.path.join(os.path.dirname(__file__), FOLDER)
-        for filename in os.listdir(route_dir):
-            if filename.endswith(".py") and filename != "__init__.py":
-                module_name = filename[:-3]
-                module = importlib.import_module(f"{FOLDER}.{module_name}")
-                if hasattr(module, "router"):
-                    try:
-                        app.include_router(module.router)
-                    except Exception as e:
-                        print(f"Lỗi khi include router2: {e}")
-
-        for subfolder in os.listdir(route_dir):
-            subfolder_path = os.path.join(route_dir, subfolder)
-            if os.path.isdir(subfolder_path):
-                for filename in os.listdir(subfolder_path):
-                    if filename.endswith(".py") and filename != "__init__.py":
-                        module_name = filename[:-3]
-                        module = importlib.import_module(f"{FOLDER}.{subfolder}.{module_name}")
-                        if hasattr(module, "router"):
-                            try:
-                                app.include_router(module.router, prefix=f"/{FOLDER}/{subfolder}")
-                            except Exception as e:
-                                print(f"Lỗi khi include router2: {e}")
+    pass
 
 FOLDERS = ["api"]
-run_router(FOLDERS)
+run_router(app, FOLDERS)
 
