0ef5b806f59ffaf4b301e9a8bc28d7c156e8b9de
Mc-None-Vn <letloc3621@gmail.com>
2025-04-19 16:50:02 +0700

    update

--- api/root.py
+++ api/root.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import json
+import os
 
 router = APIRouter()
 templates = Jinja2Templates(directory="../website")
 
 @router.get("/")
 async def read_root(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    with open(os.path.join(os.path.dirname(__file__), "../storage/data.json")) as f:
+        data = json.load(f)
+    return templates.TemplateResponse("home.html", {"request": request, "data": data})
